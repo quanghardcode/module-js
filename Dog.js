9c1bb7cd64be971ed6776d62b572bb2c7ee84335
quanghardcode <hnquang3294@gmail.com>
2019-06-03 00:14:26 +0700

    Commit index

--- Dog.js
+++ Dog.js
@@ -1,6 +1,15 @@
-function Dog(){
+const chalk = require('chalk');
+
+function Dog(name){
 	this.stomach = [];
+	this.name = name;
 }
 Dog.prototype.eat = function(cat){
 	this.stomach.push(cat);
 };
+
+Dog.prototype.sayHi = function(){
+	console.log('Hi ! I\'am a Dog. My name is ' + chalk.red(this.name));
+};
+
+module.exports = Dog;
