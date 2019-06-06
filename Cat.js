b5cd37ca7317eeb1e8456b830dd684da8051bf62
quanghardcode <hnquang3294@gmail.com>
2019-06-06 23:19:31 +0700

    improve cate eat dog

--- Cat.js
+++ Cat.js
@@ -1,7 +1,13 @@
+var Mouse = require('./Mouse');
 function Cat(){
 	this.stomach = [];
 }
-Cat.prototype.eat = function(Mouse){
-	this.stomach.push(Mouse);
+Cat.prototype.eat = function(animal){
+	if(animal instanceof Mouse){
+		this.stomach.push(animal);
+	}else{
+		throw  new Error('Cat can only eat mouse');
+	}
+	
 };
 module.exports = Cat;
